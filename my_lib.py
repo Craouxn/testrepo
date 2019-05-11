caa1864478c60ca1ec8e30ce8d0dbe285ba0a79a
Craouxn <50454630+Craouxn@users.noreply.github.com>
2019-05-11 21:37:50 +0200

    project now calls from my_lib

---
+++ my_lib.py
@@ -0,0 +1,22 @@
+def my_function():
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+
+    height = np.round(np.random.normal(1.75, 0.2, 5000), 2)
+    weight = np.round(np.random.normal(60, 15, 5000), 2)
+    np_city = np.column_stack((height, weight))
+    np_city
+
+    np.corrcoef(np_city[:,0], np_city[:,1])
+    np.std(np_city[:,0])
+
+
+    plt.title("heights distribution in city")
+    plt.xlabel("height")
+    plt.ylabel("amount")
+    plt.yticks([0, 500, 1000])
+
+    plt.hist(height, bins = [1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.22, 2.50])
+    x = plt.show()
+    return x
