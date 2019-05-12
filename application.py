b0e5c0a1913f612741148eebdc1e1b029b9b4e30
Craouxn <50454630+Craouxn@users.noreply.github.com>
2019-05-12 14:12:46 +0200

    flask creating lists via routes and sessions

--- application.py
+++ application.py
@@ -7,12 +7,16 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if not "notes" in session:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
-    return render_template("index.html", notes=notes)
+        session["notes"].append(note)
+    return render_template("index.html", notes=session["notes"])
     #return my_function()
+
+#@app.route("/hello")
+#def hello():
+#    return render_template("hello.html")
